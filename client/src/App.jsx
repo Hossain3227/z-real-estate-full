00b05cf16af347af8504cafc2467db47e683cfb6
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-04 02:34:59 +0600

    api js done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,25 +9,31 @@ import Hero from "./components/Hero/Hero";
 import Residencies from "./components/Residencies/Residencies";
 import Value from "./components/Value/Value";
 import Website from "./pages/website";
-import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import Properties from "./pages/Properties/Properties";
-
+import { QueryClient, QueryClientProvider } from "react-query";
+import {ReactQueryDevTools} from "react-query/devtools"
+import { ToastContainer } from "react-toastify";
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <BrowserRouter>
-    <Suspense fallback={<div>Loading...</div>}>
-    <Routes>
-      <Route element={<Layout/>}>
-      <Route path="/" element={<Website/>} />
-      <Route path="/properties" element={<Properties/>} />
-      </Route>
-      
-    {/* <Website></Website> */}
-    </Routes>
-    </Suspense>
-    </BrowserRouter>
-    
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Website />} />
+              <Route path="/properties" element={<Properties />} />
+            </Route>
+
+            {/* <Website></Website> */}
+          </Routes>
+        </Suspense>
+      </BrowserRouter>
+      <ToastContainer/>
+      <ReactQueryDevTools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
