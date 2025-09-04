88ca63bfc1430402fcf4b8e73006996645b002b9
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-05 01:53:55 +0600

    property page half done

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -4,7 +4,9 @@ import { useQuery } from 'react-query'
 import { useLocation } from 'react-router-dom'
 import { getProperty } from '../../utils/api'
 import { PuffLoader } from 'react-spinners'
-import { AiFillHeart } from 'react-icons/ai'
+import { AiFillHeart, AiTwotoneCar } from 'react-icons/ai'
+import { MdMeetingRoom } from 'react-icons/md'
+import { FaShower } from 'react-icons/fa'
 const Property = () => {
 
 const {pathname} = useLocation()
@@ -15,7 +17,7 @@ const id = pathname.split("/").slice(-1)[0]
 
 const {data, isLoading, isError} = useQuery(["resd", id], ()=> getProperty(id))
 
-// console.log(data);
+console.log(data);
 
 if(isLoading){
     return (
@@ -46,6 +48,43 @@ if(isError){
         </div>
     {/* image */}
     <img src={data?.image} alt="home image" />
+
+    <div className="flexCenter property-details">
+        {/* left */}
+        <div className="flexColStart left">
+            {/* head */}
+            <div className="flexStart head">
+                <span className='primaryText'>{data?.title}</span>
+                <span className='orangeText' style={{fontSize:'1.5rem'}}>$ {data?.price}</span>
+            </div>
+            {/* facilities */}
+            <div className="flexStart facilities">
+                {/* Bathrooms */}
+                <div className="flexStart facility">
+                    <FaShower size={20} color="#1F3E72"/>
+                    <span>{data?.facilities?.bathroom} Bathrooms</span>
+                </div>
+                {/* parkings */}
+                <div className="flexStart facility">
+                <AiTwotoneCar size={20} color="#1F3E72"/>
+                    <span>{data?.facilities?.parkings} Parkings</span>
+                </div>
+                {/* Rooms */}
+                <div className="flexStart facility">
+                <MdMeetingRoom size={20} color="#1F3E72"/>
+                    <span>{data?.facilities?.bathrooms} Rooms</span>
+                </div>
+            </div>
+            {/* description */}
+            <span className='secondaryText' style={{textAlign: "justify"}}>
+            {data?.description}
+            </span>
+        </div>
+        {/* right */}
+        <div className="right">
+            this is right side
+        </div>
+    </div>
         </div>
     </div>
   )
