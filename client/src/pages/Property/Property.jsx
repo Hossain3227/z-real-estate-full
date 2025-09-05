db4fdd2428327055e3059e0bb36ba53ccabb82a9
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-06 03:05:36 +0600

    almost map done

--- client/src/pages/Property/Property.jsx
+++ client/src/pages/Property/Property.jsx
@@ -5,8 +5,9 @@ import { useLocation } from 'react-router-dom'
 import { getProperty } from '../../utils/api'
 import { PuffLoader } from 'react-spinners'
 import { AiFillHeart, AiTwotoneCar } from 'react-icons/ai'
-import { MdMeetingRoom } from 'react-icons/md'
+import { MdMeetingRoom, MdLocationPin} from 'react-icons/md'
 import { FaShower } from 'react-icons/fa'
+import Map from '../../components/Map/Map'
 const Property = () => {
 
 const {pathname} = useLocation()
@@ -79,10 +80,30 @@ if(isError){
             <span className='secondaryText' style={{textAlign: "justify"}}>
             {data?.description}
             </span>
+            {/* address */}
+            <div className='flexStart' style={{gap: "1rem"}}>
+                <MdLocationPin size={25}/>
+                <span className='secondaryText'>
+                {
+                    data?.address
+                }
+                {
+                    data?.city
+                }
+                {
+                    data?.country
+                }
+                </span>
+            </div>
+
+            {/* booking button */}
+            <button className="button">
+                Book your Visit
+            </button>
         </div>
         {/* right */}
-        <div className="right">
-            this is right side
+        <div className="map">
+            <Map address={data?.address} city={data?.city} country={data?.country}/>
         </div>
     </div>
         </div>
