83a68b85907a207f52096224e4ed94f09639ea5a
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-08 02:39:29 +0600

    jawt setup done

---
+++ server/config/auth0Config.js
@@ -0,0 +1,9 @@
+import {auth} from 'express-oauth2-jwt-bearer'
+
+const jwtCheck = auth({
+    audience: "http://localhost:8000",
+    issuerBaseURL: "https://dev-8l8hvg6xsjvn1bj2.us.auth0.com",
+    tokenSigningAlg: "RS256"
+})
+
+export default jwtCheck
--- server/routes/residencyRoute.js
+++ server/routes/residencyRoute.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import { createUser } from '../controllers/userCntrl.js'
 import { createResidency, getAllResidencies, getResidency } from '../controllers/resdCntrl.js'
+import jwtCheck from '../config/auth0Config.js'
 
 const router = express.Router()
 
-router.post("/create", createResidency)
+router.post("/create", jwtCheck,createResidency)
 router.get("/allresd", getAllResidencies)
 router.get("/:id",getResidency)
 
