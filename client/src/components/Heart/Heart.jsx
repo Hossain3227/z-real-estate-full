6a3ab14a3049668b42956c39623e90a59176cb27
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-10 19:46:23 +0600

    user detail added in heart comp

--- client/src/components/Heart/Heart.jsx
+++ client/src/components/Heart/Heart.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import useAuthCheck from '../../hooks/useAuthCheck'
+import { AiFillHeart } from 'react-icons/ai'
+import { useMutation } from 'react-query'
+import { useAuth0 } from '@auth0/auth0-react'
+import UserDetailContext from '../../Context/UserDetailContext'
 
-const Heart = () => {
+const Heart = ({id}) => {
+
+    const [heartColor,setHeartColor] = useState("white")
+    const {validateLogin} = useAuthCheck();
+    const {user} = useAuth0()
+    const {userDetails : {token, bookings}, setUserDetails} = useContext(UserDetailContext);
+
+    const {mutate} = useMutation({
+        mutationFn: ()=> toFav(id,user?.email,token)
+    })
+
+    const handleLike = () => {
+        if(validateLogin()){
+            mutate()
+            setHeartColor((prev)=> prev === "fa3e5f" ? "white" : "fa3e5f")
+        }
+    }
+
   return (
-    <AiFillHeart size={24} color='white'/>
+    <AiFillHeart size={24} color={heartColor} onClick={(e)=> {
+        e.stopPropagation()
+        handleLike()
+    }}/>
   )
 }
 
