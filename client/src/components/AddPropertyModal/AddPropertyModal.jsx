fea92822609c1f8b73cd1bee02975d7d786438e8
Hossain3227 <hasan32.hk27@gmail.com>
2025-09-13 23:28:52 +0600

    add property modal initial setup done

--- client/src/components/AddPropertyModal/AddPropertyModal.jsx
+++ client/src/components/AddPropertyModal/AddPropertyModal.jsx
@@ -1,17 +1,39 @@
-import { Modal } from '@mantine/core'
-import React from 'react'
+import { Container, Modal, Stepper } from "@mantine/core";
+import React, { useState } from "react";
+import AddLocation from "../AddLocation/AddLocation";
 
-const AddPropertyModal = ({opened, setOpened}) => {
+const AddPropertyModal = ({ opened, setOpened }) => {
+  const [active, setActive] = useState(0);
   return (
     <Modal
-    opened={opened}
-    onClose={()=> setOpened(false)}
-    closeOnClickOutside
-    size={"90rem"}
+      opened={opened}
+      onClose={() => setOpened(false)}
+      closeOnClickOutside
+      size={"90rem"}
     >
-        Property modal
+      <Container h={"40rem"} w={"100%"}>
+        <Stepper
+          active={active}
+          onStepClick={setActive}
+          breakpoint="sm"
+          allowNextStepsSelect={false}
+        >
+          <Stepper.Step label="Location" description="Address">
+            <AddLocation/>
+          </Stepper.Step>
+          <Stepper.Step label="Second step" description="Verify email">
+            Step 2 content: Verify email
+          </Stepper.Step>
+          <Stepper.Step label="Final step" description="Get full access">
+            Step 3 content: Get full access
+          </Stepper.Step>
+          <Stepper.Completed>
+            Completed, click back button to get to previous step
+          </Stepper.Completed>
+        </Stepper>
+      </Container>
     </Modal>
-  )
-}
+  );
+};
 
-export default AddPropertyModal
+export default AddPropertyModal;
